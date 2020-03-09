cbeadfa730b31a4b73296efb71e44224eb1a18a0
Adrian Nikiel <hunter86@onet.eu>
2020-03-09 21:21:37 +0100

    added values to properties and to Rectangular Reader

--- src/main/java/pl/anikiel/rectangleapp/RectangularReader.java
+++ src/main/java/pl/anikiel/rectangleapp/RectangularReader.java
@@ -1,7 +1,34 @@
 package pl.anikiel.rectangleapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RectangularReader {
+
+    @Value("${siteA1}")
+    private int siteA1;
+
+    @Value("${siteA2}")
+    private int siteA2;
+
+    @Value("${siteA3}")
+    private int siteA3;
+
+    @Value("${siteA4}")
+    private int siteA4;
+
+    @Value("${siteB1}")
+    private int siteB1;
+
+    @Value("${siteB2}")
+    private int siteB2;
+
+    @Value("${siteB3}")
+    private int siteB3;
+
+    @Value("${siteB4}")
+    private int siteB4;
+
+    
 }
