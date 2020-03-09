db86c90dc84f38b16a0843c772719c65937eabfb
Adrian Nikiel <hunter86@onet.eu>
2020-03-09 20:09:10 +0100

    added dependencies after changing version of vaadin

---
+++ src/test/java/pl/anikiel/rectangleapp/RectangleappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.anikiel.rectangleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RectangleappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
