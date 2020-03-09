31938d5c0dc9eabb48b152dd023d9d9d9d4b64ab
Adrian Nikiel <hunter86@onet.eu>
2020-03-09 19:48:05 +0100

    added fields to RectanguleGui and initialize them in constructor

--- src/main/java/pl/anikiel/rectangleapp/RectangleGui.java
+++ src/main/java/pl/anikiel/rectangleapp/RectangleGui.java
@@ -1,7 +1,24 @@
 package pl.anikiel.rectangleapp;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 @Route
-public class RectangleGui {
+public class RectangleGui extends VerticalLayout {
+
+    private TextField textFieldHeight;
+    private TextField textFieldWidth;
+    private Button button;
+
+    public RectangleGui() {
+         textFieldHeight = new TextField("Podaj wysokość");
+         textFieldWidth = new TextField("Podaj szerokość");
+         button = new Button("Dodaj!");
+
+         add(textFieldHeight);
+         add(textFieldWidth);
+         add(button);
+    }
 }
