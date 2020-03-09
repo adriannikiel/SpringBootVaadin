71c4facbd71a708daf3ce1ef54def04773c6b6fe
Adrian Nikiel <hunter86@onet.eu>
2020-03-09 17:26:40 +0100

    added SpringBootApplication

---
+++ src/main/java/pl/anikiel/rectangleapp/RectangleappApplication.java
@@ -0,0 +1,13 @@
+package pl.anikiel.rectangleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RectangleappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RectangleappApplication.class, args);
+    }
+
+}
