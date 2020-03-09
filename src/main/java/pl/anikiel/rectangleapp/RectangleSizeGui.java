736b48deefd6ea0e496416090016a35a252b3db9
Adrian Nikiel <hunter86@onet.eu>
2020-03-09 22:10:00 +0100

    added gui for big and small rectangules

--- src/main/java/pl/anikiel/rectangleapp/RectangleSizeGui.java
+++ src/main/java/pl/anikiel/rectangleapp/RectangleSizeGui.java
@@ -1,6 +1,25 @@
 package pl.anikiel.rectangleapp;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
 
+@Route
 public class RectangleSizeGui extends VerticalLayout {
+
+    private RectangleRepo rectangleRepo;
+
+    private TextArea textAreaRectangles;
+
+    @Autowired
+    public RectangleSizeGui(RectangleRepo rectangleRepo) {
+        this.rectangleRepo = rectangleRepo;
+
+        textAreaRectangles = new TextArea();
+
+        textAreaRectangles.setValue(rectangleRepo.getBigRectangles().toString());
+
+        add(textAreaRectangles);
+    }
 }
